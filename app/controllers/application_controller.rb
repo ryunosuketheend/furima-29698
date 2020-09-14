001986fc68bc3190d8e10796c36053ca628fc950
ryunosuketheend <ryu.always.onemu@gmail.com>
2020-09-14 20:22:31 +0900

    新規登録&ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :basic_auth
 
+  # private
+
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["FURIMA_USER"] && password == ENV["FURIMA_PASSWORD"]
+  #   end
+  # end
   private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["FURIMA_USER"] && password == ENV["FURIMA_PASSWORD"]
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_katakana, :last_name_katakana, :birthday ])
   end
 end
